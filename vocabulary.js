2ecb00c7eeec562101c14d13d07ef0a0f17c84a1
christian <134725438+483D8B@users.noreply.github.com>
2024-03-06 22:21:06 +0100

    added vocabulary file

---
+++ vocabulary.js
@@ -0,0 +1,20 @@
+let vocabulary = {
+    '一': [
+        { word: '一　いち', translation: 'one' },
+        { word: '一つ　ひとつ', translation: 'one' },
+        { word: '一時　いちじ', translation: 'one o\'clock' },
+        { word: '一月　いちがつ', translation: 'January' },
+        { word: '一分　いっぷん', translation: 'one minute' }
+    ],
+    '月': [
+        { word: '', translation: '' },
+        { word: '', translation: '' },
+        { word: '', translation: '' }
+    ],
+    '木': [
+        { word: '', translation: '' },
+        { word: '', translation: '' },
+        { word: '', translation: 'Thrsday' }
+    ],
+
+};
